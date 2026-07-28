c2fe7469917df099402917e1856de1717be76c9b
Thales Rangel <peritotjba@gmail.com>
2026-07-28 01:19:58 -0300

    Refactor: conectar app ao database layer

--- backend/app.js
+++ backend/app.js
@@ -3,22 +3,9 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 
-const { PrismaClient } = require('@prisma/client');
-const { PrismaPg } = require('@prisma/adapter-pg');
-const { Pool } = require('pg');
+const prisma = require('./database/prisma');
 
 const routes = require('./routes');
-
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-});
-
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({
-    adapter
-});
 
 
 const app = express();
