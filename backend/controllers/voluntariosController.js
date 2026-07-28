bafb1b80fc04a9b66c4641d056f8246c09e821f1
Thales Rangel <peritotjba@gmail.com>
2026-07-28 01:36:43 -0300

    Refactor: criar controller voluntarios

---
+++ backend/controllers/voluntariosController.js
@@ -0,0 +1,57 @@
+module.exports = function(prisma) {
+
+    return async function(req, res) {
+
+        try {
+
+            const { numero } = req.body;
+
+
+            if (!numero) {
+
+                return res.status(400).json({
+                    error: "Número obrigatório."
+                });
+
+            }
+
+
+            const voluntario =
+                await prisma.voluntario.create({
+
+                    data: {
+                        numero,
+                        saldo: 10
+                    }
+
+                });
+
+
+            return res.json({
+
+                success: true,
+
+                voluntario,
+
+                mensagem:
+                "Voluntário cadastrado com 10 horas iniciais!"
+
+            });
+
+
+        } catch (error) {
+
+
+            return res.status(400).json({
+
+                error:
+                "Número já cadastrado no sistema."
+
+            });
+
+
+        }
+
+    };
+
+};
--- backend/routes/voluntarios.js
+++ backend/routes/voluntarios.js
@@ -1,41 +1,18 @@
 const router = require('express').Router();
 
+const voluntariosController =
+    require('../controllers/voluntariosController');
+
+
 module.exports = function(prisma) {
 
-    router.post('/', async (req, res) => {
 
-        try {
+    router.post(
+        '/',
+        voluntariosController(prisma)
+    );
 
-            const { numero } = req.body;
-
-            if (!numero) {
-                return res.status(400).json({
-                    error: "Número obrigatório."
-                });
-            }
-
-            const voluntario = await prisma.voluntario.create({
-                data: {
-                    numero,
-                    saldo: 10
-                }
-            });
-
-            return res.json({
-                success: true,
-                voluntario,
-                mensagem: "Voluntário cadastrado com 10 horas iniciais!"
-            });
-
-        } catch (error) {
-
-            return res.status(400).json({
-                error: "Número já cadastrado no sistema."
-            });
-
-        }
-
-    });
 
     return router;
+
 };
